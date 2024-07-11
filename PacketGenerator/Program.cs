a947df0e548e48018b1debd624c0081baae7ff59
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2024-07-11 21:08:28 +0900

    xml 추가 xmlreader로 파싱

--- PacketGenerator/Program.cs
+++ PacketGenerator/Program.cs
@@ -1,10 +1,30 @@
 ﻿
+using System.Xml;
+
 namespace PacketGenerator
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            XmlReaderSettings settings = new XmlReaderSettings()
+            {
+                IgnoreComments = true,
+                IgnoreWhitespace = true,
+            };
+
+            using (XmlReader r = XmlReader.Create("PDL.xml", settings))
+            {
+                r.MoveToContent();//헤더를 건너뜀 
+
+                while (r.Read())//stream방식으로 read-> 1줄씩 
+                {
+
+                    Console.WriteLine(r.Name+" " + r["name"]);
+                }
+            }
+
 
         }
     }
