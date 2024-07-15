38c0a5f7193279a28646ad18092d0230d1c2582b
HEO NAMGYU <46082941+Zvckaya@users.noreply.github.com>
2024-07-15 21:25:45 +0900

    parsepacket and parsemember추가

--- PacketGenerator/Program.cs
+++ PacketGenerator/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Security.Cryptography.X509Certificates;
 using System.Xml;
 
 namespace PacketGenerator
@@ -21,11 +22,69 @@ namespace PacketGenerator
                 while (r.Read())//stream방식으로 read-> 1줄씩 
                 {
 
-                    Console.WriteLine(r.Name+" " + r["name"]);
+                    if (r.Depth == 1 && r.NodeType == XmlNodeType.Element)//endelement parsing 방지
+                        ParsePacket(r);
+                    
+                    //Console.WriteLine(r.Name+" " + r["name"]);
                 }
             }
 
+        }
 
+        public static void ParsePacket(XmlReader r)
+        {
+            if (r.NodeType == XmlNodeType.EndElement)
+                return;
+            if (r.Name.ToLower() != "packet")
+                return;
+
+            string packetName = r["name"];
+            if (string.IsNullOrEmpty(packetName))
+            {
+                Console.WriteLine("Packet without name");
+                return;
+            }
+
+            ParseMenber(r);
+        }
+
+        private static void ParseMenber(XmlReader r) //정보 긁기
+        {
+            string packetName = r["name"];
+
+            int depth = r.Depth + 1; // 깊이 1-> playerInfoReq 깊이 2 실제 long string 형 변수
+            while (r.Read())
+            {
+                if (r.Depth != depth)
+                    break;
+
+                string memberName = r["name"];
+                if (string.IsNullOrEmpty(memberName))
+                {
+                    Console.WriteLine("member witout name");
+                    return;
+                }
+
+                string memberType = r.Name.ToLower();
+                switch (memberType)
+                {
+                    case "bool":
+                    case "byte":
+                    case "short":
+                    case "ushort":
+                    case "int":
+                    case "long":
+                    case "float":
+                    case "double":
+                    case "string":
+                    case "list":
+                        break;
+                    default:
+                        break;
+
+                }
+
+            }
         }
     }
 }
